e07cc8ae06ef5cc6fc1dbf7b1df8f695130365e3
zfy <1696621890@qq.com>
2015-03-25 21:51:08 +0800

    123

--- index.php
+++ index.php
@@ -5,5 +5,45 @@
  * Date: 15/3/25
  * Time: 下午9:16
  */
-
+$item=$_POST['action'];
+switch($item){
+    case 'user':operateUser();break;
+    case 'activity':operateActivity();break;
+    case 'schedule':operateSchedule();break;
+    case 'contacts':operateContacts();break;
+}
+function operateUser(){
+    $order=$_POST[''];
+    switch($order){
+        case 'del':break;
+        case 'new':break;
+        case 'upd':break;
+        case 'get':break;
+    }
+}
+function operateActivity(){
+    $order=$_POST[''];
+    switch($order){
+        case 'del':break;
+        case 'new':break;
+        case 'reset':break;
+    }
+}
+function operateSchedule(){
+    $order=$_POST[''];
+    switch($order){
+        case 'get':break;
+        case 'del':break;
+        case 'setacti':break;
+    }
+}
+function operateContacts(){
+     $order=$_POST[''];
+    switch($order){
+        case 'get':break;
+        case 'del':break;
+        case 'add':break;
+    }
+}
+?>
 
