eea9bcfa6a80a837dae507d913fab990f75e708b
gaoyangyiqiao <gyyq1994@gmail.com>
2015-04-21 01:36:49 +0800

    。

---
+++ businesslogic/uploadMyInfo.php
@@ -0,0 +1,19 @@
+<?php
+
+	$root_path=dirname(dirname(__FILE__));
+	require($root_path.'/controller/UserController.php');
+	function saveInfo($id=0,$name,$photopath,$phone,$password=0){
+		echo 'it is in saveInfo--->>>';
+		$userInfo=array(
+			'id'=>$id,
+			'name'=>$name,
+			'photopath'=>$photopath,
+			'phone'=>$phone,
+			'password'=>$password
+			);
+
+		addUser($userInfo);
+
+	}
+
+?>
