499b6e5aa60a175957fbd6e19a60f01ce838db9f
gaoyangyiqiao <gyyq1994@gmail.com>
2015-05-31 09:39:57 +0800

    blogic

---
+++ businesslogic/addActivity.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaoyang
+ * Date: 15/5/25
+ * Time: 上午8:03
+ */
+namespace bl;
+
+
+$root_path=dirname(dirname(__FILE__));
+require($root_path.'/controller/ActivityController.php');
+function addActivity($theme,$content,$founder_id,$receive_time,$begin_time,$end_time,$contacts_id,$right){
+
+    $activity=array(
+        'id'=>0,
+        'theme'=>$theme,
+        'content'=>$content,
+        'founder'=>$founder_id,
+        'receive_time'=>$receive_time,
+        'begin_time'=>$begin_time,
+        'end_time'=>$end_time,
+        'contacts_id'=>$contacts_id,
+        'right'=>$right
+    );
+
+    return \controller\addActivity($activity);
+}
--- businesslogic/uploadMyInfo.php
+++ businesslogic/uploadMyInfo.php
@@ -14,6 +14,7 @@ namespace bl;
 			);
 
         $user_id=\controller\addUser($userInfo);
+        return $user_id;
 
 	}
 
